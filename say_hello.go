5dd529ffe7676eb0e6866b742f81bb9b3265c928
maiingonly <maherboleax@gmail.com>
2021-06-19 01:53:25 +0700

    update say_hello and calc

--- say_hello.go
+++ say_hello.go
@@ -3,3 +3,14 @@ package go_say_hello
 func SayHello() string {
 	return "hello maiing"
 }
+
+type SumCalc interface {
+}
+
+type Value struct {
+	a, b, c int
+}
+
+func (n Value) multiplication() int {
+	return n.a * n.b * n.c
+}
