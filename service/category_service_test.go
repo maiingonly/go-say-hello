631347a141aaf13439f6767eb95c5bc8703a6a08
maiingonly <maherboleax@gmail.com>
2021-07-01 16:50:14 +0700

    mock solution for golang unit test also add package entity,repository, service

---
+++ service/category_service_test.go
@@ -0,0 +1,23 @@
+package service
+
+import (
+	"testing"
+
+	"github.com/maiingonly/go-say-hello/repository"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+)
+
+var categoryRepository = &repository.CategoryRepositoryMock{Mock: mock.Mock{}}
+var categoryService = CategoryService{Repository: categoryRepository}
+
+func TestCategoryService_Get(t *testing.T) {
+
+	// progam mock
+	categoryRepository.Mock.On("FindById", "1").Return(nil)
+
+	category, err := categoryService.Get("1")
+	assert.Nil(t, category)
+	assert.NotNil(t, err)
+
+}
